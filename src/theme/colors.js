8dd77b9f499847208dfb5c5471208b4acf25948e
ADlv20 <adwaitg02@gmail.com>
2022-12-25 00:24:58 +0530

    Setting up folder structure

---
+++ src/theme/colors.js
@@ -0,0 +1,15 @@
+const colors = {
+  primary: '#14B0C9',
+  secondary: '#84B484',
+  textPrimary: '#0F0A39',
+  textTertiary: '#76787B',
+  textGrey: '#B4B4B4',
+  borderColor: '#D4D4D4',
+  backgroundColor: '#E4E4E4',
+  ratingColor: '#EBC307',
+  progressGrey: '#C1C1C1',
+  white: '#FFFFFF',
+  black: '#000000',
+  gray: '#F9F9F9',
+};
+export default colors;
